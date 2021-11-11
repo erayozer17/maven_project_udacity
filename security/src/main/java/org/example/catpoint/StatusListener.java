e91fb29693d3098cf56f37e05a747cd43c5fe2b8
Eray Özer <erayozer91@protonmail.com>
2021-11-11 21:32:52 +0100

    add modules

---
+++ security/src/main/java/org/example/catpoint/StatusListener.java
@@ -0,0 +1,10 @@
+package org.example.catpoint;
+
+/**
+ * Identifies a component that should be notified whenever the system status changes
+ */
+public interface StatusListener {
+    void notify(AlarmStatus status);
+    void catDetected(boolean catDetected);
+    void sensorStatusChanged();
+}
